8bb443189700bd28fd65abd3c625ef3a59114ca5
HendrixDev13 <155911784+HendrixDev13@users.noreply.github.com>
2024-06-12 21:20:18 -0600

    Update ConectionDB.java
    SOLO CAMBIE LA BASE DE DATOS POR EL EJEMPLO

--- ConectionDB.java
+++ ConectionDB.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class ConectionDB {
 //	
 	 public static Connection getConnection() throws SQLException {
-	        String url = "jdbc:mysql://localhost:3306/library";
+	        String url = "jdbc:mysql://localhost:3306/TUBASEDEDATOS";
 	        String usuario = "root";
-	        String contraseña = "123456789";
+	        String contraseña = "1234";
 	        
 	        Connection conexión = DriverManager.getConnection(url, usuario, contraseña);
 	        return conexión;
